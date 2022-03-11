100c27af28945d1c9aa5979c6a9097925c000160
Aaron Crawford <1900400@uad.ac.uk>
2022-03-11 01:18:03 +0000

    Created the classes needed

---
+++ Source/BehaviourTree/AIPatrol.cpp
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AIPatrol.h"
+
+// Sets default values
+AAIPatrol::AAIPatrol()
+{
+
+}
+
+// Called when the game starts or when spawned
+void AAIPatrol::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called to bind functionality to input
+void AAIPatrol::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
